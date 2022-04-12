18006116f6b51c3b0c7a16d43d8d60a73bcb2dcf
Nela <usr@nela.moe>
2022-04-12 19:16:13 +0200

    minor temporary fix.

--- install-gbs-control.sh
+++ install-gbs-control.sh
@@ -1,25 +1,19 @@
 #!/bin/ash
 # Install script for Trueview 5725 control (GBS8200, GBS8220, HD9000, HD Box Pro etc)
 
-DIR=$HOME
+DIR=$HOME/
 echo -e "\nInstall location is: "$DIR
 
 # Update sources and install I2C components.
 echo -e "\nUpdating sources & installing i2c utils:"
 sudo apt-get update
-sudo apt-get install -y i2c-tools libi2c-dev python3-smbus
+sudo apt-get install -y i2c-tools libi2c-dev python3-smbus git
 
 # Get latest stable version from GitHub
 echo -e "\nDownloading current master version:"
 cd $DIR
-wget https://raw.githubusercontent.com/paranoidbashthot/gbs-control/master/gbs-control.zip
-
-# Unpack scripts & default settings
-echo -e "\nUnpacking zip package:"
-unzip -oq $DIR/gbs-control.zip
-
-echo -e "\nRemove zip package:"
-rm $DIR/gbs-control.zip
+git clone https://github.com/paranoidbashthot/gbs-control.git .
+# this is ulrasketch, soon will be replaced with a more proper solution
 
 # Patch /etc/inittab to allow for automatic login.
 # and to use xterm-mono for B&W (monochrome) interactive terminal.
